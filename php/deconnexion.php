8a37ee19155c26855b12726f676a4be83379ec1c
Thomastlf <ttaillefer12@gmail.com>
2026-04-05 23:00:08 +0200

    Update deconnexion.php

--- php/deconnexion.php
+++ php/deconnexion.php
@@ -1,5 +1,5 @@
 <?php
 session_start(); 
-session_unset(); // On détruit toutes les données de la session
-header('Location: http://localhost:8000/php/connexion.php'); // Retour à l'accueil ou au login
+session_unset(); 
+header('Location: http://localhost:8000/php/connexion.php'); 
 ?>
