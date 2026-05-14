868f87fc4dcd290ee042338e8de7656719561e0c
Rafaeldst <rafael.doussaint@gmail.com>
2026-05-14 12:29:51 +0200

    Update profil.php

--- php/profil.php
+++ php/profil.php
@@ -17,7 +17,8 @@ $role_session = isset($_SESSION['role']) ? strtolower($_SESSION['role']) : 'clie
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/profil.css">
+    <link rel="stylesheet" href="../css/global.css">
+    <link rel="stylesheet" href="../css/perso.css">
     <link rel="icon" type="image/png" href="../img/Logo_Tasty_Country.png">
     <title>Profil - Tasty Country</title>
     <style>
