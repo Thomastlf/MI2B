952ff4b33618041c865810805911393acc94ed81
Rafaeldst <rafael.doussaint@gmail.com>
2026-05-14 11:25:14 +0200

    Add cookie management for dark mode toggle

--- js/connexion.js
+++ js/connexion.js
@@ -1,17 +1,32 @@
 const bouton = document.getElementById("bouton");
 const css = document.getElementById("css");
-let modeSombre = false;
 
-//fonction qui change le mdoe
-function changerStatut() {
-    if (modeSombre) {
+function setCookie(cookieName, cookieValue, expiration = null) {// fonction qui créer le cookie
+    if(expiration == null) expiration = new Date(Date.now() + 86400000).toUTCString();
+        document.cookie = cookieName + "=" + cookieValue +"; expires=" + expiration + ";";
+}
+
+function getCookie(cookieName, defaultValue = null) {// fonction qui récupère le cookie
+    const cookies = document.cookie.split(";");
+    let row = cookies.find((row) => row.trim().startsWith(cookieName + "="));
+    if(row == null) return defaultValue;
+        return row.split("=")[1];
+}
+
+function changerStatut() {//fonction qui change le mode (clair/sombre)
+    if (getCookie("modeSombre")=="true") {
         css.setAttribute("href", "../css/connexion.css");
         bouton.innerHTML = "Passer en sombre";
-        modeSombre = false;
+        setCookie("modeSombre", "false");
     } else {
-        css.setAttribute("href", "../css/connexion_sombre.css");
+        css.setAttribute("href", "../css_sombre/connexion.css");
         bouton.innerHTML = "Passer en clair";
-        modeSombre = true;
+        setCookie("modeSombre", "true");
     }
 }
+
+if (getCookie("modeSombre") == null) {//On créer le cookie s'il n'existe pas déjà
+    setCookie("modeSombre", "false");
+}
+
 bouton.addEventListener("click", changerStatut);
