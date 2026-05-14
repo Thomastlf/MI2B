1532eaf3d70a59bc585e55fc35a3c697198c12ba
Rafaeldst <rafael.doussaint@gmail.com>
2026-05-14 12:28:10 +0200

    Add global CSS stylesheet to inscription.php

--- php/inscription.php
+++ php/inscription.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/global.css">
     <link rel="stylesheet" href="../css/inscription.css">
     <link rel="icon" type="image/png" href="../img/Logo_Tasty_Country.png">
     <title>Inscription - Tasty Country</title>
