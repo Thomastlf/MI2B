b09ca736331cad343cb57dc5da641975adf1ce7b
Rafaeldst <rafael.doussaint@gmail.com>
2026-05-15 11:56:48 +0200

    Update connexion.js

--- js/connexion.js
+++ js/connexion.js
@@ -1,6 +1,7 @@
 const envoyer = document.getElementById("envoyer");
 const champEmail = document.getElementById("email");
 const champMdp = document.getElementById("mdp");
+const erreur_js = document.getElementById("erreur_js");
 
 envoyer.onsubmit = function (e) {//evenement qui se déclenche lorsque le formulaire est envoyé et qui bloque l'envoie du formulaire vers le serveur si les champs sont invalides
     let email=champEmail.value;
@@ -15,8 +16,15 @@ envoyer.onsubmit = function (e) {//evenement qui se déclenche lorsque le formul
             contient_point=true;
         }
     }
-
     if (contient_arobase == false || contient_point == false) {
         e.preventDefault();//bloque l'envoie du formulaire
+        erreur_js.innerHTML = "Format invalide : l'email doit contenir un '@' et un '.'.";
+    }
+    else if (mdp.length<8){
+        e.preventDefault();//bloque l'envoie du formulaire
+        erreur_js.innerHTML = "Format invalide : le mot de passe doit avoir une longueur d'au moins 8 caractères.";
+    }
+    else{
+        erreur_js.innerHTML = "";
     }
 };
