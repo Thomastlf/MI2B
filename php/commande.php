2bb03a8271994aeed6caecf346233a5af5474616
Rafaeldst <rafael.doussaint@gmail.com>
2026-05-17 16:51:36 +0200

    Update commande.php

--- php/commande.php
+++ php/commande.php
@@ -64,6 +64,12 @@ foreach ($commandes as $c) {
         $livrees[] = $c;
     }
 }
+$css="";
+$texteBouton="Passer en mode malvoyant";
+if(isset($_COOKIE["theme"]) && $_COOKIE["theme"] == "true"){
+    $css="../css/theme.css";
+    $texteBouton="Passer en mode par défaut";
+}
 ?>
 
 <!DOCTYPE html>
@@ -75,6 +81,8 @@ foreach ($commandes as $c) {
     <link rel="stylesheet" type="text/css" href="../css/commande.css">
     <link rel="icon" type="image/png" href="../img/Logo_Tasty_Country.png">
     <title>Espace Restaurateur - Tasty Country</title>
+    <link id="css" rel="stylesheet" href=<?php echo $css; ?>><!-- js -->
+    <script src="../js/theme.js" defer></script><!-- js / defer pour n'exécuter le script js qu'une fois que le navigateur aura chargé le html dans le dom -->
 </head>
 <body>
     <div class="site-container">
@@ -102,6 +110,7 @@ foreach ($commandes as $c) {
                     </ol>
                 </nav>
             </div>
+            <button id="bouton" class="btn_theme"><?php echo $texteBouton; ?></button><!-- js -->
         </header>
 
         <main class="content">
