47a77401f5f77c213b6caac1ffa88106f457cfe1
Rafaeldst <rafael.doussaint@gmail.com>
2026-05-17 16:52:05 +0200

    Update notation.php

--- php/notation.php
+++ php/notation.php
@@ -62,6 +62,12 @@ if (!empty($_POST) && !$deja_note) {
     $deja_note = true;
     $messageSucces = "Merci ! Votre carnet de bord a été enregistré avec succès. ✈️";
 }
+$css="";
+$texteBouton="Passer en mode malvoyant";
+if(isset($_COOKIE["theme"]) && $_COOKIE["theme"] == "true"){
+    $css="../css/theme.css";
+    $texteBouton="Passer en mode par défaut";
+}
 ?>
 
 <!DOCTYPE html>
@@ -72,9 +78,11 @@ if (!empty($_POST) && !$deja_note) {
     <link rel="stylesheet" href="../css/global.css">
     <link rel="stylesheet" href="../css/notation.css">
     <link rel="icon" type="image/png" href="../img/Logo_Tasty_Country.png">
+    <link id="css" rel="stylesheet" href=<?php echo $css; ?>><!-- js -->
     <title>Notation - Tasty Country</title>
     
     <script src="../js/notation.js" defer></script>
+    <script src="../js/theme.js" defer></script><!-- js / defer pour n'exécuter le script js qu'une fois que le navigateur aura chargé le html dans le dom -->
 </head>
 <body>
     <div class="site-container">
@@ -92,6 +100,7 @@ if (!empty($_POST) && !$deja_note) {
                     </ol>
                 </nav>
             </div>
+            <button id="bouton" class="btn_theme"><?php echo $texteBouton; ?></button><!-- js -->
         </header>
 
         <main class="content">
