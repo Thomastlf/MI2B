2f5ca350f8c4307340eec690db5e159c6678df71
Rafaeldst <rafael.doussaint@gmail.com>
2026-05-17 14:48:43 +0200

    Update maj_profil.php

--- php/maj_profil.php
+++ php/maj_profil.php
@@ -5,11 +5,31 @@ $utilisateurs = json_decode(file_get_contents("../json/utilisateur.json"), true)
 
 foreach ($utilisateurs as $index => $ligne) {/*on prend l'index car sinon le tableau que l'on modifie n'est qu'une copie*/
     if ($ligne['email'] == $_SESSION['email']) {
-        $utilisateurs[$index]['nom']=$_GET['nom'];
-        $utilisateurs[$index]['prenom']=$_GET['prenom'];
-        $utilisateurs[$index]['adresse']=$_GET['adresse'];
-        $utilisateurs[$index]['code_interphone']=$_GET['code'];
-        $utilisateurs[$index]['numero']=$_GET['numero'];
+        if ($_GET['nom']!="") {
+            $utilisateurs[$index]['nom']=$_GET['nom'];
+        }
+        if ($_GET['prenom']!="") {
+            $utilisateurs[$index]['prenom']=$_GET['prenom'];
+        }
+        if ($_GET['email']!="") {
+            $utilisateurs[$index]['email']=$_GET['email'];
+            $_SESSION['email'] = $_GET['email'];
+        }
+        if ($_GET['adresse']!="") {
+            $utilisateurs[$index]['adresse']=$_GET['adresse'];
+        }
+        if ($_GET['code']!="") {
+            $utilisateurs[$index]['code_interphone']=$_GET['code'];
+        }
+        if ($_GET['numero']!="") {
+            $utilisateurs[$index]['numero']=$_GET['numero'];
+        }
+        if ($_GET['date']!="") {
+            $utilisateurs[$index]['date']=$_GET['date'];
+        }
+        if ($_GET['genre']!="") {
+            $utilisateurs[$index]['genre']=$_GET['genre'];
+        }
     }
 }
 
