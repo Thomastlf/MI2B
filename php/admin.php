84f670625d7c4f727bc147ed7bd0847c8918116d
Rafaeldst <rafael.doussaint@gmail.com>
2026-05-31 16:49:47 +0200

    Update admin.php

--- php/admin.php
+++ php/admin.php
@@ -1,6 +1,15 @@
 <?php
 session_start();
-
+if (isset($_SESSION['email'])) {
+    $data = json_decode(file_get_contents("../json/utilisateur.json"), true);
+    foreach ($data as $ligne) {
+        if ($_SESSION['email'] == $ligne['email'] && $ligne['statut'] == 'Bloqué') {
+            session_destroy();
+            header("Location: accueil.php");
+            exit();
+        }
+    }
+}
 if (!isset($_SESSION['role']) || strtolower($_SESSION['role']) !== 'admin') {
     header("Location: accueil.php");
     exit();
