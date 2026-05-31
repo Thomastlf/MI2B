ef940f773956fd9b219a6320bbdf6070a8dbd673
Rafaeldst <rafael.doussaint@gmail.com>
2026-05-31 16:43:13 +0200

    Create admin.js

---
+++ js/admin.js
@@ -0,0 +1,19 @@
+const serv = "http://localhost:8000/";
+
+async function bloquer(email, bouton) {
+    try {
+        const response = await fetch(serv + "php/maj_admin.php?" + "email="+email);
+        if (response.ok) {
+            const nouveauStatut = await response.text();
+            if (nouveauStatut == 'Bloqué') {
+                bouton.innerHTML = '🚫';
+            } else if (nouveauStatut == 'Actif') {
+                bouton.innerHTML = '✅';
+            }
+        } else {
+            console.error("La requête n'a pas abouti : " + response.status);
+        }
+    } catch (e) {
+        console.error("Erreur avec fetch", e);
+    }
+}
